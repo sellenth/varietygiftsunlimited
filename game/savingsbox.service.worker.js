43e68c24f1e7bf4d0f1f88184ee16d66bb79036e
Halston Sellentin <hsellentin@hygiena.com>
2024-09-15 21:48:43 -0700

    new

--- game/savingsbox.service.worker.js
+++ game/savingsbox.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1726460900|541281931';
+const CACHE_VERSION = '1726462094|1734993712';
 /** @type {string} */
 const CACHE_PREFIX = 'savingsbox-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
