8ed91fc6a48f15db32b075aa0087cd54b6c25092
Halston Sellentin <hsellentin@hygiena.com>
2024-10-13 21:25:13 -0700

    broken build...

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
