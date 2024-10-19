50e9bbe15c3b12d0242bfdf3b7406f4645fbd172
Halston <halstonsellentin@gmail.com>
2024-10-19 13:35:18 -0700

    take2

---
+++ astro.config-server.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind()],
+  vite: {
+    plugins: [ basicSsl() ]
+  }
+});
