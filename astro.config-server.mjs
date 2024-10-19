445fe14b79f70ffccd513b197a9794b53c5768fc
Halston <halstonsellentin@gmail.com>
2024-10-19 13:47:04 -0700

    trying to build

--- astro.config-server.mjs
+++ astro.config-server.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://astro.build/config
 export default defineConfig({
