1ca9f516aa82344b70cf6939217e47100eff853b
Halston <halstonsellentin@gmail.com>
2024-10-28 23:18:42 -0700

    fix build

--- src/scripts/yt.ts
+++ src/scripts/yt.ts
@@ -1,5 +1,3 @@
-import Game from "./game";
-
 /** * Copyright 2024 Google LLC * * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * * https://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */ /** * The top-level namespace for the YouTube Playables SDK. */ declare namespace ytgame {
   /** * The types of errors that the YouTube Playables SDK throws. */ export const enum SdkErrorType {
     /** * The error type is unknown. */ UNKNOWN,
@@ -345,8 +343,7 @@ function runAnimation() {
 }
 
 function drawGame() {
-  const game = new Game();
-  game.animate();
+  // TODO: draw game
 }
 
 /**
