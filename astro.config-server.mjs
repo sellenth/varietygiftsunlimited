bb90179aee83afc02bab623df799ec6a1c31abb2
Halston <halstonsellentin@gmail.com>
2025-03-15 20:47:32 -0700

    uninstalls

--- astro.config-server.mjs
+++ astro.config-server.mjs
@@ -2,25 +2,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   integrations: [react(), tailwind()],
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
-  vite: {
-    ssr: {
-      noExternal: ['zwitch', 'hast-util-to-html'],
-    },
-    resolve: {
-      alias: {
-        'zwitch': 'zwitch/index.js'
-      }
-    }
-  }
 });
