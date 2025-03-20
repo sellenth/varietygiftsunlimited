02182933c2f7e1bcf779523d5f5003f005e8c801
Halston <halstonsellentin@gmail.com>
2025-03-20 07:24:43 -0700

    global links

---
+++ src/data/socials.ts
@@ -0,0 +1,5 @@
+export const socials = {
+  instagram: "https://www.instagram.com/wvw.variety.gifts/",
+  youtube: "https://www.youtube.com/@varietygifts",
+  tiktok: "https://www.tiktok.com/@varietygiftsunlimited"
+}; 
