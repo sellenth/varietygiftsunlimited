836ce1575fbb430026952b8299a771c28b446bd6
Halston <halstonsellentin@gmail.com>
2025-03-29 16:37:16 -0700

    bags added

---
+++ src/data/bagsData.ts
@@ -0,0 +1,15 @@
+export const bagNames = [
+    "Red Lines Bag",
+    "Zig Zag Bag",
+    "Red Tassle Bag",
+    "Yellow Flower Bag",
+    "Grey Stripe Bag",
+    "Sheep Flock Bag",
+    "Red Smile Bag",
+    "Brown Bear Bag",
+    "Purple Flower Bag",
+    "Red Zig Zag Bag",
+    "Green Smile Bag",
+    "Daisy Bag",
+    "Grey Smile Bag"
+]; 
