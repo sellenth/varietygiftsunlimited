36d7663dd9929aaa13a82f3873f797ed6c8c4634
halston <halstonsellentin@gmail.com>
2025-09-14 21:10:07 -0700

    fingers crosst

--- astro.config-server.mjs
+++ astro.config-server.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/server";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+  adapter: vercel(),
   integrations: [react(), tailwind()],
 });
