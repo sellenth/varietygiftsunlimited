2c45150a1cf2d8cad539599bdbfcbb93d323838e
halston <halstonsellentin@gmail.com>
2025-10-13 19:24:30 -0700

    trying sitemap

--- astro.config-server.mjs
+++ astro.config-server.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.variety.gifts",
   output: 'server',
   adapter: vercel({}),
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
 });
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 //import wasm from 'vite-plugin-wasm'
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.variety.gifts",
   devToolbar: {
     enabled: false
   },
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   vite: {
     plugins: [basicSsl()],
   },
